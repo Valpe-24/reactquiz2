2ff44ac2d08a6c99d1a918a4972973f8060bccff
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-05-30 10:26:40 -0400

    Quiz

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const AddCalculator = () => {
+    let [counter, setCounter] = useState(0);
+
+    const handleClick = (addNumber) => {
+
+        if (addNumber === '1'){
+            setCounter(++counter)
+        } else if(addNumber ==='2'){
+            setCounter(counter += 2)
+        }else{
+            setCounter(counter+=3)
+        }
+
+    }
 
     return (
         <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
+            <button onClick={() => handleClick('1')}>Add 1</button>
+            <button onClick={() => handleClick('2')}>Add 2</button>
+            <button onClick={() => handleClick('3')}>Add 3</button>
             <br></br>
-            Total: 
+            Total: {counter} 
         </>
     )
 }
--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const NumberBuilder = () => {
+    let [numberString, setNumberString] = useState('');
+    let [counter, setCounter] = useState(0);
+    
+    const handleClick= (parameter) => {
+        if(parameter === 'add'){
+            setCounter(++counter); 
+            setNumberString(numberString += `${counter.toString()} `)
+        }else if(parameter === 'reset'){
+            setCounter(0);
+            setNumberString('');
+        }
+    }
 
     return (
         <>
-            <button>Add Number</button>
-            <button>Reset</button>
+            <button onClick={()=>handleClick('add')}>Add Number</button>
+            <button onClick={()=>handleClick('reset')}>Reset</button>
             <br></br>
-            Numbers: 
+            Numbers: {numberString}
         </>
     )
 }
